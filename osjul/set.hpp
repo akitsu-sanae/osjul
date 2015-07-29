f06671043b311ea3f48841e5c19ee4b67f9c02c6
akitsu-sanae <akitsu.sanae@gmail.com>
2015-07-29 22:00:31 +0900

    add set

---
+++ osjul/set.hpp
@@ -0,0 +1,11 @@
+#ifndef OSJUL_SET_HPP
+#define OSJUL_SET_HPP
+
+#include <osjul/set/set.hpp>
+
+#endif
+
+
+
+
+
---
+++ osjul/set/set.hpp
@@ -0,0 +1,60 @@
+#ifndef OSJUL_SET_SET_HPP
+#define OSJUL_SET_SET_HPP
+
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <osjul/identity.hpp>
+
+namespace osjul {
+
+template<typename T, typename Con = std::vector<T>>
+class set {
+    using this_type = set<T,Con>;
+public:
+    using value_type = T;
+    using container_type = Con;
+
+    set(container_type const& con) :
+        m_data(con)
+    {}
+
+    template<typename U>
+    set(typename U::iterator beginIt,typename  U::iterator endIt) :
+        m_data(beginIt, endIt)
+    {}
+
+    set(std::initializer_list<value_type> list) :
+        m_data(list.begin(),list.end())
+    {}
+
+    container_type& get() { return m_data; }
+
+    friend this_type&
+    operator>> (
+            this_type& set_,
+            typename osjul::identity<std::function<value_type (value_type)>>::type f) {
+        for (auto&& e : set_.m_data)
+            e = f(e);
+        return set_;
+    }
+
+    friend this_type&
+    operator|| (
+            this_type& set_,
+            typename osjul::identity<std::function<bool (value_type)>>::type f) {
+        auto it = std::remove_if(
+                set_.m_data.begin(),
+                set_.m_data.end(),
+                std::not1(f));
+        set_.m_data.erase(it,set_.m_data.end());
+        return set_;
+    }
+
+private:
+    container_type m_data;
+};
+
+}
+
+#endif
