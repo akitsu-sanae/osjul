8b199dac7125c89e7ddb347bbb863d6277e08023
akitsu-sanae <akitsu.sanae@gmail.com>
2016-03-01 23:47:39 +0900

    fix maybe

--- osjul/maybe.hpp
+++ osjul/maybe.hpp
@@ -1,10 +1,10 @@
 #ifndef OSJUL_MAYBE_HPP
 #define OSJUL_MAYBE_HPP
 
-namespace tken {
+namespace osjul {
 
-template<template<typename> class T, typename U, typename Func>
-T<U>& operator>=(T<U>& e, Func const& f) {
+template<template<typename ...> class T, typename U, typename Func>
+decltype(auto) operator>(T<U>& e, Func const& f) {
     if (e)
         e = f(*e);
     return e;
