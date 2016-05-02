c9c5d13471015da13df10bb884b0b4127e2bacc6
akitsu-sanae <akitsu.sanae@gmail.com>
2016-05-02 18:52:40 +0900

    change lisence

--- osjul/identity.hpp
+++ osjul/identity.hpp
@@ -1,3 +1,12 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+
+
 #ifndef OSJUL_IDENTITY_HPP
 #define OSJUL_IDENTITY_HPP
 
--- osjul/string.hpp
+++ osjul/string.hpp
@@ -1,3 +1,11 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+
 #ifndef OSJUL_STRING_STRING_HPP
 #define OSJUL_STRING_STRING_HPP
 
