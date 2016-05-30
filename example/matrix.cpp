c6dc6e2a98de0bff61d45259e9dd65ae5ebbe6cc
akitsu-sanae <akitsu.sanae@gmail.com>
2016-05-30 21:27:18 +0900

    add mult matrix

--- example/matrix.cpp
+++ example/matrix.cpp
@@ -6,5 +6,39 @@ int main() {
         {1, 0},
         {0, 1}
     }};
+    osjul::la::matrix<double, 1, 2> n {{
+        {4},
+        {2}
+    }};
+    std::cout << m * n << std::endl;
+
+
+    osjul::la::matrix<double, 2, 2> a {{
+        {2, -1},
+        {-3, 4}
+    }};
+    osjul::la::matrix<double, 1, 2> b {{
+        {1}, {2}
+    }};
+    std::cout << a*b << std::endl;
+
+    osjul::la::matrix<double, 2, 2> c {{
+        {1, -1},
+        {-2, 3}
+    }};
+    osjul::la::matrix<double, 2, 2> d {{
+        {1, 2},
+        {3, 4}
+    }};
+    std::cout << c * d << std::endl;
+
+    osjul::la::matrix<double, 2, 1> e {{
+        {1, 2}
+    }};
+    osjul::la::matrix<double, 2, 2> f {{
+        {2, -1},
+        {-3, 4}
+    }};
+    std::cout << e * f << std::endl;
 }
 
--- osjul/liner_algebra/matrix.hpp
+++ osjul/liner_algebra/matrix.hpp
@@ -10,6 +10,7 @@
 
 #include <cstddef>
 #include <array>
+#include <iostream>
 
 namespace osjul {
 namespace la {
@@ -17,12 +18,65 @@ namespace la {
 template<typename T, std::size_t W, std::size_t H>
 struct matrix {
     using value_type = T;
-    static constexpr std::size_t N = W;
-    static constexpr std::size_t M = H;
+    static constexpr std::size_t Width = W;
+    static constexpr std::size_t Height = H;
+    using this_type = matrix<value_type, Width, Height>;
+
+    struct invalid_index_exception {};
+
+    value_type const& at(int x, int y) const {
+        if (x < 0 || x > Width)
+            throw invalid_index_exception{};
+        if (y < 0 || y > Height)
+            throw invalid_index_exception{};
+        return elems[y][x];
+    }
+    value_type& at(int x, int y) {
+        if (x < 0 || x > Width)
+            throw invalid_index_exception{};
+        if (y < 0 || y > Height)
+            throw invalid_index_exception{};
+
+        return elems[y][x];
+    }
+
+    this_type operator+(this_type const& rhs) const {
+        this_type result = *this;
+        for (int y=0; y<Height; y++) {
+            for (int x=0; x<Width; x++) {
+                result.at(x, y) = rhs.at(x, y);
+            }
+        }
+        return result;
+    }
+
+    template<std::size_t I>
+    matrix<value_type, I, Height> operator*(matrix<value_type, I, Width> const& rhs) const {
+        matrix<value_type, I, Height> result = {};
+        for (int y=0; y<Height; y++) {
+            for (int x=0; x<I; x++) {
+                for (int i=0; i<Width; i++)
+                    result.at(x, y) += this->at(i, y) * rhs.at(x, i);
+            }
+        }
+        return result;
+    }
 
 
-    value_type elems[N][M];
+    value_type elems[Height][Width];
 };
+
+template<typename T, std::size_t W, std::size_t H>
+inline static std::ostream& operator<<(std::ostream& os, matrix<T, W, H> const& m) {
+    os << W << " x " << H << " matrix" << std::endl;
+    for (std::size_t y=0; y<H; y++) {
+        std::cout << "    ";
+        for (std::size_t x=0; x<W; x++) {
+            std::cout << m.at(x, y) << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 
 }
