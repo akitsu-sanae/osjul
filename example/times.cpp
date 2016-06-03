0d1b8ce21647899cb1b5b08d8f00b34544745ca0
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-03 19:33:47 +0900

    add times

---
+++ example/times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "../osjul/times.hpp"
+
+
+int main() {
+    using namespace osjul;
+    10_times
+        > [](int i) { std::cout << i << std::endl; }
+        > [](int i) { std::cout << i*2 << std::endl; }
+        ;
+}
+
---
+++ osjul/times.hpp
@@ -0,0 +1,37 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+#ifndef OSJUL_TIMES_HPP
+#define OSJUL_TIMES_HPP
+
+namespace osjul {
+
+namespace detail {
+
+struct times_impl {
+    unsigned long long n;
+    template<typename F>
+    times_impl const& operator>(F const& f) const {
+        for (unsigned long long i=0; i<n; i++)
+            f(i);
+        return *this;
+    }
+};
+
+
+}
+
+inline static auto operator "" _times(unsigned long long n) {
+    return detail::times_impl{n};
+}
+
+} // namespace osjul
+
+#endif
+
+
+
