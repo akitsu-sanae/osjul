ba51f19a49ecd9dfd85a8467d3d20e6f0ebbede3
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-07 16:39:39 +0900

    add type_traits.any_of

---
+++ example/any_of.cpp
@@ -0,0 +1,11 @@
+#include <osjul/type_traits/any_of.hpp>
+#include <type_traits>
+#include <string>
+#include <iostream>
+
+int main() {
+    std::cout << std::boolalpha;
+    std::cout << osjul::type_traits::any_of<std::is_const, int, double, std::string, int>::value << std::endl;
+    std::cout << osjul::type_traits::any_of<std::is_const, int, const std::string, double>::value << std::endl;
+}
+
---
+++ osjul/type_traits/any_of.hpp
@@ -0,0 +1,35 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+#ifndef OSJIL_TYPE_TRAITS_ANY_OF_HPP
+#define OSJIL_TYPE_TRAITS_ANY_OF_HPP
+
+#include <type_traits>
+
+namespace osjul {
+namespace type_traits {
+
+template<template<typename> class Pred, typename Head, typename ... Tail>
+struct any_of :
+    std::integral_constant<bool,
+        Pred<Head>::value ?
+            true :
+            any_of<Pred, Tail ...>::value
+        >
+{};
+
+template<template<typename> class Pred, typename Head>
+struct any_of<Pred, Head> :
+    std::integral_constant<bool, Pred<Head>::value>
+{};
+
+
+}
+}
+
+#endif
+
