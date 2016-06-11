64a9b60ba995d7220eded3b248393c36a0c53fb5
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-11 22:31:27 +0900

    fix

--- osjul/any.hpp
+++ osjul/any.hpp
@@ -1,6 +1,6 @@
 /*============================================================================
   Copyright (C) 2015-2016 akitsu sanae
-  https://github.com/akitsu-sanae/geko
+  https://github.com/akitsu-sanae/osjul
   Distributed under the Boost Software License, Version 1.0. (See accompanying
   file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 ============================================================================*/
