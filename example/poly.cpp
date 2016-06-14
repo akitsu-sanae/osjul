8fa0d3a0e27e53fe66870b2b3db0872dd22dc167
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-14 14:03:16 +0900

    add poly.eval and poly.uniform

--- example/poly.cpp
+++ example/poly.cpp
@@ -10,16 +10,40 @@
 
 int main() {
     using namespace osjul::math;
+
+    // (12*y) + x
     auto term1 = std::make_shared<term>(
             std::make_shared<term>(
-                std::make_shared<number<int>>(12),
+                std::make_shared<number>(12),
                 '*',
                 std::make_shared<variable>("y")
                 ),
             '+',
             std::make_shared<variable>("x")
             );
-    std::cout << term1->to_string() << std::endl;
-    std::cout << term1->subst("y", std::make_shared<number<int>>(23)).to_string() << std::endl;
+    std::cout << "             : " << term1->to_string() << std::endl;
+    std::cout << "subst y:=23 => " << term1->subst("y", std::make_shared<number>(23)).to_string() << std::endl;
+    std::cout << "eval        => " << term1->eval()->to_string() << std::endl;
+
+    std::cout << "--------------------------------------------------" << std::endl;
+
+    // (x+y) * (x+y)
+    auto term2 = std::make_shared<term>(
+            std::make_shared<term>(
+                std::make_shared<variable>("x"),
+                '+',
+                std::make_shared<variable>("y")
+                ),
+            '*',
+            std::make_shared<term>(
+                std::make_shared<variable>("x"),
+                '+',
+                std::make_shared<variable>("y")
+                )
+            );
+    std::cout << "            : " << term2->to_string() << std::endl;
+    std::cout << "uniform    => " << term2->uniform().to_string() << std::endl;
+    std::cout << "subst y:=3 => " << term2->subst("y", std::make_shared<number>(3)).to_string() << std::endl;
+    std::cout << "eval       => " << term2->eval()->to_string() << std::endl;
 }
 
--- osjul/math/polynomial.hpp
+++ osjul/math/polynomial.hpp
@@ -29,20 +29,21 @@ struct variable : public expression {
     std::string name;
 };
 
-template<typename T>
 struct number : public expression {
-    using value_type = T;
-
-    number(value_type const& v) :
+    number(int const& v) :
         value(v)
     {}
     std::string to_string() const override {
         return std::to_string(value);
     }
-    value_type value;
+    int value;
 };
 
-struct term : public expression {
+#define NUMBER_PTR(x) (std::dynamic_pointer_cast<number>(x))
+#define VARIABLE_PTR(x) (std::dynamic_pointer_cast<variable>(x))
+#define TERM_PTR(x) (std::dynamic_pointer_cast<term>(x))
+
+struct term : public expression, std::enable_shared_from_this<term> {
     term(std::shared_ptr<expression> const& l, char c, std::shared_ptr<expression> const& r) :
         left(l),
         op(c),
@@ -50,12 +51,12 @@ struct term : public expression {
     {}
     std::string to_string() const override {
         std::string result;
-        if (auto&& t = std::dynamic_pointer_cast<term>(left))
+        if (auto&& t = TERM_PTR(left))
             result += "(" + left->to_string() + ")";
         else
             result += left->to_string();
         result += op;
-        if (auto&& t = std::dynamic_pointer_cast<term>(right))
+        if (auto&& t = TERM_PTR(right))
             result += "(" + right->to_string() + ")";
         else
             result += right->to_string();
@@ -63,9 +64,9 @@ struct term : public expression {
     }
     term& subst(std::string const& variable_name, std::shared_ptr<expression> const& expr) {
         auto subst_ = [&](std::shared_ptr<expression>& body) {
-            if (auto&& t = std::dynamic_pointer_cast<term>(body))
+            if (auto&& t = TERM_PTR(body))
                 t->subst(variable_name, expr);
-            else if (auto&& v = std::dynamic_pointer_cast<variable>(body)) {
+            else if (auto&& v = VARIABLE_PTR(body)) {
                 if (v->name == variable_name)
                     body = expr;
             }
@@ -74,10 +75,91 @@ struct term : public expression {
         subst_(right);
         return *this;
     }
+    struct invalid_operation { char op_code; };
+
+    std::shared_ptr<expression> eval() {
+        if (auto&& t = TERM_PTR(left))
+            left = t->eval();
+        if (auto&& t = TERM_PTR(right))
+            right = t->eval();
+
+        auto&& left_n = NUMBER_PTR(left);
+        auto&& right_n = NUMBER_PTR(right);
+        if (left_n && right_n)
+            return eval_impl(left_n, right_n);
+        return shared_from_this();
+    }
+
+    term& uniform() {
+        if (op != '*' && op != '/')
+            return *this;
+        auto left_term = TERM_PTR(left);
+        if (left_term)
+            return uniform_impl(left_term, right);
+        auto right_term = TERM_PTR(right);
+        if (right_term)
+            return uniform_impl(right_term, left);
+        return *this;
+    }
+
+
     std::shared_ptr<expression> left;
     char op;
     std::shared_ptr<expression> right;
+
+private:
+
+    // 12 + 34 => 46
+    std::shared_ptr<expression> eval_impl(
+            std::shared_ptr<number> const& left,
+            std::shared_ptr<number> const& right) const
+    {
+        int value = 0;
+        switch (op) {
+        case '+':
+            value = left->value + right->value;
+            break;
+        case '-':
+            value = left->value - right->value;
+            break;
+        case '*':
+            value = left->value * right->value;
+            break;
+        case '/':
+            value = left->value / right->value;
+        default:
+            throw invalid_operation { op };
+        }
+        return std::make_shared<number>(value);
+    }
+
+    // (a + 12) * y => (a*y) + (12 * y)
+    term& uniform_impl(
+            std::shared_ptr<term> const& left_term,
+            std::shared_ptr<expression> const& right_expr)
+    {
+        auto left_tmp = std::make_shared<term>(
+                left_term->left,
+                op,
+                right_expr);
+        auto right_tmp = std::make_shared<term>(
+                left_term->right,
+                op,
+                right_expr);
+
+        left_tmp->uniform();
+        right_tmp->uniform();
+
+        op = left_term->op;
+        left = left_tmp;
+        right = right_tmp;
+        return *this;
+    }
 };
+
+#undef TERM_PTR
+#undef VARIABLE_PTR
+#undef NUMBER_PTR
 
 } // namespace math
 } // namespace osjul
